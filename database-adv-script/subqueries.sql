c57706fab0111d6b8238d71b9b8dd310d47419cd
Keithclinton <keithnyaburi@gmail.com>
2025-05-14 13:22:52 +0300

    scriptss

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,31 @@
+-- Query 4: Non-correlated subquery to find all properties where the average rating is greater than 4.0
+SELECT 
+    properties.id AS property_id,
+    properties.name AS property_name
+FROM 
+    properties
+WHERE 
+    properties.id IN (
+        SELECT 
+            reviews.property_id
+        FROM 
+            reviews
+        GROUP BY 
+            reviews.property_id
+        HAVING 
+            AVG(reviews.rating) > 4.0
+    );
+
+-- Query 5: Correlated subquery to find users who have made more than 3 bookings
+SELECT 
+    users.id AS user_id,
+    users.name AS user_name
+FROM 
+    users
+WHERE 
+    (SELECT 
+         COUNT(*)
+     FROM 
+         bookings
+     WHERE 
+         bookings.user_id = users.id) > 3;
